d70955884a0c08c92a52798fb2f42cb5b3039beb
Sameishchev Anton <anton.sameishchev@devexpress.com>
2017-04-10 18:55:18 +0300

    Progress Saving added
    But ProgressLoading not

--- bot.py
+++ bot.py
@@ -39,6 +39,28 @@ class SQLighter:
         """ Получаем одну строку с номером rownum """
         with self.connection:
             return self.cursor.execute('SELECT * FROM Players WHERE id = ?', (rownum,)).fetchall()[0]
+        
+    def create_new_player(self, username, starttime):
+        with self.connection:
+            self.cursor.execute('INSERT INTO Players(Name, Time_Gaming) VALUES(?,?)', (username, starttime))
+            
+    def add_team(self, username, team):
+        with self.connection:
+           self.cursor.execute('UPDATE Players SET Team = ? WHERE Name = ?', (team, username))
+
+    def player_exists(self, name):
+        with self.connection:
+           return self.cursor.execute('SELECT EXISTS(SELECT * FROM Players WHERE Name = ?)', (name,)).fetchone()
+           
+    def read_last_code(self, name):
+        """ Получаем одну строку с номером rownum """
+        with self.connection:
+            return self.cursor.execute('SELECT Last_Answered_Question FROM Players WHERE Name = ?', (name,)).fetchall()[0]
+
+    def write_last_code(self, name, code):
+        """ Получаем одну строку с номером rownum """
+        with self.connection:
+            self.cursor.execute('UPDATE Players SET Last_Answered_Question = ? WHERE Name = ?', (code, name))
 
     def count_rows(self):
         """ Считаем количество строк """
@@ -63,7 +85,7 @@ def send_music(message, name):
             f = open('media/'+file, 'rb')
             res = bot.send_voice(message.chat.id, f, None)
             print(res)
-        time.sleep(2)
+        time.sleep(1)
 
 @bot.message_handler(commands=['testpicture'])
 def send_picture(message, name):
@@ -72,23 +94,87 @@ def send_picture(message, name):
             f = open('media/'+file, 'rb')
             res = bot.send_photo(message.chat.id, f, None)
             print(res)
-        time.sleep(2)
+        time.sleep(1)
         
 @bot.message_handler(commands=['game'])
 def game(message):
     # Подключаемся к БД
+    chat_id = message.chat.id
+    user = user_dict[chat_id]
     db_worker = SQLighter(config.database_name)
     # Получаем случайную строку из БД
     #row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
-    row = db_worker.select_single(2)
+    team_name=user.team
+    if team_name == "Максим":
+        row = db_worker.select_single(2)
+    if team_name =="Кирилл":
+        row = db_worker.select_single(1)
     # Формируем разметку
-    markup = utils.generate_markup(row[1], row[2])
-    # Отправляем аудиофайл с вариантами ответа
+    #markup = utils.generate_markup(row[1], row[2])
+    # Отправляем тестовое сообщение
     bot.send_message(message.chat.id, row[1]+' '+str(row[2]))
     # Включаем "игровой режим"
    #utils.set_user_game(message.chat.id, row[2])
     # Отсоединяемся от БД
     db_worker.close()
+
+def save_code_to_base (chatid, user, code):
+    db_worker = SQLighter(config.database_name)
+    chat_id = chatid
+    user = user_dict[chat_id]
+    team_name=user.team
+    #if team_name == "Максим":
+    bot.send_message(chat_id, str(code))
+    db_worker.close()
+    row = db_worker.write_last_code(team_name, code)
+    #if team_name =="Кирилл":
+   #     row = db_worker.select_single(user, code)
+   
+@bot.message_handler(commands=['jump'])
+def read_code_from_base (message):
+       chat_id = message.chat.id
+       user = user_dict[chat_id]
+       #chat_id = message
+       db_worker = SQLighter(config.database_name)
+      # user = user_dict[message.chat.id]
+       team_name=user.team
+       #if team_name == "Максим":
+       row = db_worker.read_last_code(team_name)
+       db_worker.close()
+       bot.send_message(message, str(row))
+
+def create_new_player (chatid, username, starttime):
+       #chat_id = message.chat.id
+       #chat_id = message
+       db_worker = SQLighter(config.database_name)
+      # user = user_dict[message.chat.id]
+       #team_name=user.team
+       #if team_name == "Максим":
+       row = db_worker.create_new_player(username, starttime)
+       db_worker.close()
+       
+def add_team (chatid, username, team):
+       #chat_id = message.chat.id
+       #chat_id = message
+       db_worker = SQLighter(config.database_name)
+      # user = user_dict[message.chat.id]
+       #team_name=user.team
+       #if team_name == "Максим":
+       row = db_worker.create_new_player(username, team)
+       db_worker.close()
+                        
+def player_exists (name):
+       #chat_id = message.chat.id
+       #chat_id = message
+       db_worker = SQLighter(config.database_name)
+      # user = user_dict[message.chat.id]
+       #team_name=user.team
+       #if team_name == "Максим":
+       return db_worker.player_exists(name)
+       
+       db_worker.close()
+
+
 
     
 # Handle '/start' and '/help'
@@ -103,11 +189,22 @@ def send_welcome(message):
     currentquestion = 0
     global starttime
     starttime=message.date
-    msg = bot.send_message(chat_id, """\
+    username=message.chat.username
+    bot.send_message(chat_id, username)
+    bot.send_message(chat_id, str(player_exists(username)))
+    if player_exists("sdfsdf")==0:
+        bot.send_message(chat_id, str(player_exists(username)))
+        create_new_player(chat_id, message.chat.username, starttime)
+        msg = bot.send_message(chat_id, """\
 Добрый день!
 Как тебя зовут?
 """)
-    bot.register_next_step_handler(msg, process_team_step)
+        bot.register_next_step_handler(msg, process_team_step)
+    else:        
+        bot.send_message(chat_id, str(player_exists(username)))
+        msg = bot.send_message(chat_id,"Привет, "+message.chat.first_name+"! Давно не виделись! Напиши, как тебя зовут сегодня?")
+        bot.register_next_step_handler(msg, process_team_step)
+    
 
 def process_team_step(message):
     try:
@@ -131,6 +228,7 @@ def process_choose_step(message):
         user = user_dict[chat_id]
         if (team == u'Кирилл') or (team == u'Максим'):
             user.team = team
+            add_team(message.chat.username, team)
         else:
             raise Exception()
         bot.send_message(chat_id, 'Удачной игры ' + user.name + '\nТы в команде:' + user.team)        
@@ -165,11 +263,15 @@ def game(message):
     global currentquestion
     global codeindex
     global hintcount
+    chat_id = message.chat.id
+    user = user_dict[chat_id]
     codeindex=codes.index(message.text)
     bot.send_message(message.chat.id, "Код №"+str(codeindex+1)+" принят!")
     if codeindex == 0:
         send_picture(message, 'photo')
         give_next_question(message, codeindex)
+       # bot.send_message(message.chat.id, "пупок")
+        save_code_to_base(chat_id, user, str(codes[codeindex]))
        #answerssum +=codeindex
         
 #    questionindex=questions.index(codeindex)
